c26e4871e2915388a356cd05391cb8f1e04f3c01
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-06-28 15:03:22 +0600

    learnt casting

---
+++ casting.py
@@ -0,0 +1,13 @@
+# There may be times when you want to specify a type on to a variable. This can be done with casting.
+
+x = int(1)
+y = int(2.9)
+z = int("7")
+
+x = str("e1")
+y = str(6)
+z = str(5.0)
+
+x = float(1.2)
+z = float("7")
+w = float("4.2")
